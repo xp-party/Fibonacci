02ab3416080b75515ca5d2638ff64a694b21b0dd
Vitaly Ruzhnikov <varuzhnikov@gmail.com>
2012-05-11 15:31:40 +0400

    initial commit

---
+++ Fibonacci/Fibonacci.h
@@ -0,0 +1,16 @@
+//
+//  Fibonacci.h
+//  Fibonacci
+//
+//  Created by Виталий on 11.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Fibonacci_Fibonacci_h
+#define Fibonacci_Fibonacci_h
+static int MAX_FIB_COUNT = 46;
+
+int fib(int index);
+
+
+#endif
