1ad2c3df910fb8743a090d9010f1c04b91c044d8
Vitaly Ruzhnikov <varuzhnikov@gmail.com>
2012-05-15 14:27:28 +0400

    Template for unit tests made

---
+++ FibonacciUnitTests/XPFibonacciTest.h
@@ -0,0 +1,13 @@
+//
+//  XPFibonacciTest.h
+//  Fibonacci
+//
+//  Created by Виталий on 15.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XPFibonacciTest : SenTestCase
+
+@end
---
+++ FibonacciUnitTests/XPFibonacciTest.m
@@ -0,0 +1,17 @@
+//
+//  XPFibonacciTest.m
+//  Fibonacci
+//
+//  Created by Виталий on 15.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XPFibonacciTest.h"
+
+@implementation XPFibonacciTest
+
+- (void)testYourFirstTest {
+    STAssertTrue(YES, @"should pass");
+}
+
+@end
